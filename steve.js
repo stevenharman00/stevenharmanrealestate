42b9de3ebb06711e0310ab03bce13a1903aba663
stevenharman00 <71852251+stevenharman00@users.noreply.github.com>
2020-09-29 21:03:07 -1000

    Add files via upload

---
+++ steve.js
@@ -0,0 +1,9 @@
+var mood = prompt("How are you doing today?");
+if (mood === "good" || mood === "great" || mood === "awesome" || mood === "sweet") {
+	alert("awesome");
+}
+else {
+	alert("Well that sucks!");
+}
+
+
